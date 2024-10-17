b6b4516a4eb92f111e1a4128e857b7d1a81601cc
Idir Lankri <idir.lankri@geneanet.org>
2024-10-17 11:49:52 +0200

    Plugin_api: Disable timeout for the endpoint `API_IMAGE_ALL`

--- src/plugin_api.ml
+++ src/plugin_api.ml
@@ -109,7 +109,7 @@ let () =
     ; ( "API_INFO_IND"
       , aux @@ w_base @@ Plugin_api_lib.Api.print_info_ind)
     ; ( "API_IMAGE_ALL"
-      , aux @@ w_base @@ Plugin_api_lib.Api.print_img_all)
+      , aux ~timeout_duration:0 @@ w_base @@ Plugin_api_lib.Api.print_img_all)
     ; ( "API_IMAGE_PERSON"
       , aux @@ w_base @@ Plugin_api_lib.Api.print_img_person)
     ; ( "API_IMAGE_UPDATE"
