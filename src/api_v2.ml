70193d936b7dc76a3db602073b4231ef15bf9ea0
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2025-09-01 14:05:56 +0200

    hoist request handling

--- src/api_v2.ml
+++ src/api_v2.ml
@@ -2,14 +2,16 @@ let not_found conf = Api_util.print_error conf `not_found ""
 
 let bad_request conf = Api_util.print_error conf `bad_request ""
 
+let handle_request conf base request =
+  match Response.response conf base request with
+  | None -> not_found conf
+  | Some response ->
+    let data = Response.to_piqi response in
+    Api_util.print_result conf (Api_v2_piqi_ext.gen_person data)
+
 let handler conf base =
   let piqi_request = Api_util.get_params conf Api_v2_piqi_ext.parse_request in
   let person_request = Request.request_of_piqi_request piqi_request in
   match person_request with
-  | Some request ->
-    (match Response.response conf base request with
-    | Some response ->
-      let data = Response.to_piqi response in
-      Api_util.print_result conf (Api_v2_piqi_ext.gen_person data)
-    | None -> not_found conf)
   | None -> bad_request conf
+  | Some request -> handle_request conf base request
