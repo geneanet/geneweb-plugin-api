49d34a3ceb05b83392f15434e38496e24bd53005
Idir Lankri <idir.lankri@geneanet.org>
2025-12-11 16:32:52 +0100

    Data caches: Adapt the code to GeneWeb changes

--- src/api_saisie_autocomplete.ml
+++ src/api_saisie_autocomplete.ml
@@ -15,7 +15,7 @@ let is_valid_suggestion ~mode ~place_mode = match mode, place_mode with
     starts_with
 
 let get_list_from_cache ~conf ~base ~mode ~place_mode ~n ~ini =
-  let cache = Caches.read_cache ~conf mode in
+  let cache = Geneweb.Caches.read_cache ~conf mode in
   let ini = Name.lower @@ Ext_string.tr '_' ' ' ini in
   let is_valid = is_valid_suggestion ~mode ~place_mode in
   (* optim : on sait que la liste est triée. *)
@@ -33,5 +33,5 @@ let get_list_from_cache ~conf ~base ~mode ~place_mode ~n ~ini =
       else List.rev accu
   in
   let data = loop cache [] 0 in
-  let patch_data = Caches.complete_with_patch mode base (fun s -> is_valid ~ini ~candidate:s) data in
+  let patch_data = Geneweb.Caches.complete_with_patch mode base (fun s -> is_valid ~ini ~candidate:s) data in
   Ext_list.take (List.sort_uniq Utf8.alphabetic_order patch_data) n
