dcd61d6ec5495c55ac7917e08f4200990db39e2a
Elie Canonici Merle <elie.canonicimerle@geneanet.org>
2026-09-04 10:07:26 +0200

    reset sosa cache before generating modification status

--- src/api_saisie_write.ml
+++ src/api_saisie_write.ml
@@ -1098,6 +1098,7 @@ let compute_modification_status' conf base ip ifam resp =
   let (is_base_updated, warnings, miscs, conflict, history_records, updated_persons_infos) =
     compute_warnings conf base resp
   in
+
   (* Maintenant que l'on sait si tout s'est bien passé, *)
   (* on peut enfin commiter le fichier patch.           *)
   let () =
@@ -1108,7 +1109,7 @@ let compute_modification_status' conf base ip ifam resp =
       end
     else ()
   in
-
+  Geneweb.Sosa_cache.reset_cache ();
   let person_update, father, mother = person_updates conf base updated_persons_infos ifam in
   let response =
     {
